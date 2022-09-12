245fcbe90d150a986de10c262be801017710dd76
Pravin Kumar Mathuri <pravinkumarmathuri@gmail.com>
2022-09-12 22:08:08 +0530

    objects_in_js

---
+++ index.js
@@ -0,0 +1,15 @@
+// Rahul wants to plant 5 seeds on 5 farms.
+// for(let i=1;i<=10;i++) {
+//   let bag = "";
+// for (let j=1;j<=10;j++) {
+//   if(i==1 || i==10){
+//     bag += "*";
+//   }else if ( j==1 || j==10){
+//     bag += "*";
+//   }else {
+//     bag = bag+  " ";
+//   }
+  
+// }
+// console.log(bag);
+// }
